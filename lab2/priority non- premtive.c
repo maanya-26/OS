b42fb33da92d7b77b454c5ef34cc9f86cca5b9e4
maanya-26 <maanyasree.cs24@bmsce.ac.in>
2026-03-13 11:51:32 +0530

    Add files via upload

---
+++ lab2/priority non- premtive.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+
+struct process{
+    int pid, at, bt, pr, ct, tat, wt, rt, completed;
+};
+
+int main(){
+
+    int n,i,time=0,count=0,highest;
+    float avg_wt=0,avg_tat=0,avg_rt=0;
+
+    struct process p[20];
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++){
+        p[i].pid=i+1;
+        printf("Enter AT BT Priority for P%d: ",p[i].pid);
+        scanf("%d%d%d",&p[i].at,&p[i].bt,&p[i].pr);
+        p[i].completed=0;
+    }
+
+    while(count<n){
+
+        highest=-1;
+
+        for(i=0;i<n;i++){
+            if(p[i].at<=time && p[i].completed==0){
+                if(highest==-1 || p[i].pr < p[highest].pr){
+                    highest=i;
+                }
+            }
+        }
+
+        if(highest==-1){
+            time++;
+        }
+        else{
+
+            p[highest].rt = time - p[highest].at;
+
+            time += p[highest].bt;
+
+            p[highest].ct = time;
+
+            p[highest].tat = p[highest].ct - p[highest].at;
+
+            p[highest].wt = p[highest].tat - p[highest].bt;
+
+            avg_wt += p[highest].wt;
+            avg_tat += p[highest].tat;
+            avg_rt += p[highest].rt;
+
+            p[highest].completed=1;
+            count++;
+        }
+    }
+
+    printf("\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\tRT\n");
+
+    for(i=0;i<n;i++){
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        p[i].pid,
+        p[i].at,
+        p[i].bt,
+        p[i].pr,
+        p[i].ct,
+        p[i].tat,
+        p[i].wt,
+        p[i].rt);
+    }
+
+    printf("\nAverage TAT = %.2f",avg_tat/n);
+    printf("\nAverage WT  = %.2f",avg_wt/n);
+    printf("\nAverage RT  = %.2f\n",avg_rt/n);
+
+    return 0;
+}
